b63067762d5f03608e5a280ed1105dc27ac3d06e
zhilin1112 <chriskong1112@gmail.com>
2021-08-02 00:47:29 +0800

    update

--- Holo Library/src/holoLib/LibraryCard.java
+++ Holo Library/src/holoLib/LibraryCard.java
@@ -1,5 +1,36 @@
 package holoLib;
 
 public class LibraryCard {
+    private String memberId; 
+    private String memberExpDate; 
+
+    public LibraryCard(){
+        this("", "");
+    }
+
+    public LibraryCard(String memberId, String memberExpDate){
+        this.memberId = memberId;
+        this.memberExpDate = memberExpDate;
+    }
+
+    public void setId(String memberId){
+        this.memberId = memberId;
+    }
+
+    public void setExpDate(String memberExpDate){
+        this.memberExpDate = memberExpDate;
+    }
+
+    public String getMemberId(){
+        return memberId;
+    }
+
+    public String getMemberExpDate(){
+        return memberExpDate;
+    }
+
+    public String toString() {
+        return "Member ID: " + memberId + "\n Expired Date: " + memberExpDate + "\n";
+    }
     
 }
--- Holo Library/src/holoLib/Member.java
+++ Holo Library/src/holoLib/Member.java
@@ -1,5 +1,35 @@
 package holoLib;
 
 public class Member {
+    private String name; 
+    private LibraryCard libraryCard;
+    private static int totalMember = 0;
+    private boolean isMember; // check is it member, need?
+
+    public Member(){
+        this.name = "";
+    }
+
+    public Member(String name, LibraryCard libraryCard){
+        this.name = name;
+        totalMember++;
+     
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public static int getTotalMember(){
+        return totalMember;
+    }
+
+    private void registerMembership(){
+        
+    }
 
 }

3a96bfd0b880d1a026ebc3c2c7cf78733b314a91
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-06 14:48:21 +0800

    Updated Schedule.java

--- Holo Library/src/holoLib/Schedule.java
+++ Holo Library/src/holoLib/Schedule.java
@@ -1,5 +1,81 @@
 package holoLib;
 
 public class Schedule {
-    
+    /********** Properties **********/
+    private String scheduleID = null;
+    private String scheduleName = null;
+    private Timeslot[][] timeslots = null;
+    private static int totalSchedule = 0;
+    private static final int TOTAL_OPERATION_DAYS_PER_WEEK = 7;
+    private static final int TOTAL_TIMESLOTS_PER_DAY = 12;
+
+    /********** Constructors **********/
+    public Schedule() {
+        createTimeslots();
+        totalSchedule++;
+    }
+
+    public Schedule(String scheduleID, String scheduleName) {
+        this.scheduleID = scheduleID;
+        this.scheduleName = scheduleName;
+        createTimeslots();
+        totalSchedule++;
+    }
+
+    /********** Accessors & Mutators **********/
+    public String getScheduleID() {
+        return scheduleID;
+    }
+
+    public String getScheduleName() {
+        return scheduleName;
+    }
+
+    public static int getTotalSchedule() {
+        return totalSchedule;
+    }
+
+    public void setScheduleID(String scheduleID) {
+        this.scheduleID = scheduleID;
+    }
+
+    public void setScheduleName(String scheduleName) {
+        this.scheduleName = scheduleName;
+    }
+
+    /********** Methods **********/
+    public void createTimeslots() {
+        timeslots = new Timeslot[TOTAL_OPERATION_DAYS_PER_WEEK][TOTAL_TIMESLOTS_PER_DAY];
+        for (int i = 0; i < timeslots.length; i++) {
+            for (int j = 0; j < timeslots[i].length; j++) {
+                timeslots[i][j] = new Timeslot(new int[] { i, j });
+            }
+        }
+    }
+
+    public int checkTotalAvaiableTimeslots() {
+        int avaiableTimeSlots = 0;
+
+        for (int i = 0; i < timeslots.length; i++) {
+            for (int j = 0; j < timeslots[i].length; j++) {
+                avaiableTimeSlots += timeslots[i].length;
+
+                if (timeslots[i][j].isReserved() == true) {
+                    avaiableTimeSlots--;
+                }
+            }
+        }
+
+        return avaiableTimeSlots;
+    }
+
+    public void displaySchedule() {
+
+    }
+
+    /********** toString() method **********/
+    public String toString() {
+        return "Schedule ID: " + scheduleID + "\nSchedule Name: " + "\nTotal Available Timeslots: "
+                + checkTotalAvaiableTimeslots();
+    }
 }

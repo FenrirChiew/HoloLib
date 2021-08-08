a49634bb621933c8748012c18fc7a062701917df
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-08 13:48:47 +0800

    Updated ICTArea.java

--- Holo Library/src/holoLib/ICTArea.java
+++ Holo Library/src/holoLib/ICTArea.java
@@ -1,5 +1,46 @@
 package holoLib;
 
-public class ICTArea {
-    
+public class ICTArea extends Facility {
+    /********** Properties **********/
+    private int totalComputers = 0;
+    private Seat[] seats = null;
+    private static int totalICTArea = 0;
+
+    /********** Constructors **********/
+    public ICTArea() {
+        super();
+        totalICTArea++;
+    }
+
+    public ICTArea(String facilityName, String facilityID, String facilityType, int totalComputers) {
+        super(facilityName, facilityID, facilityType);
+        this.totalComputers = totalComputers;
+        createSeats();
+        totalICTArea++;
+    }
+
+    /********** Accessors & Mutators **********/
+    public int getTotalComputers() {
+        return totalComputers;
+    }
+
+    public void setTotalComputers(int totalComputers) {
+        this.totalComputers = totalComputers;
+    }
+
+    public Seat[] getSeats() {
+        return seats;
+    }
+
+    public static int getTotalICTArea() {
+        return totalICTArea;
+    }
+
+    /********** Methods **********/
+    public void createSeats() {
+        seats = new Seat[totalComputers];
+        for (int i = 0; i < seats.length; i++) {
+            seats[i].setSeatNO(String.format("%03d", i));
+        }
+    }
 }

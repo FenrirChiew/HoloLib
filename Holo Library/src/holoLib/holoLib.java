227329f2a2da87f54a72678eef7243e4acbe369d
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-08 23:44:17 +0800

    Add a cls() method for clear screen

--- Holo Library/src/holoLib/holoLib.java
+++ Holo Library/src/holoLib/holoLib.java
@@ -1,7 +1,21 @@
 package holoLib;
 
+import java.util.Scanner;
+
 public class holoLib {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello World!");
+        System.out.print("Press Enter to continue...");
+        sc.nextLine();
+        cls();
+
+        sc.close();
+    }
+
+    public static void cls() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }

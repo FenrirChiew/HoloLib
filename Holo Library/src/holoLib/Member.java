622ea247f814c9971610cbff5fcad1f3fcf28993
zhilin1112 <chriskong1112@gmail.com>
2021-08-08 10:25:59 +0800

    update library.java

--- Holo Library/src/holoLib/Member.java
+++ Holo Library/src/holoLib/Member.java
@@ -33,48 +33,7 @@ public class Member extends Account{
         return super.toString() + libraryCard.toString() ;
     }
 
-    // Register Membership method 
-    public void registerMembership(){
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Do you want to register a membership (Y= yes N= No)? ");
-        char confirm = sc.nextLine().charAt(0);
-
-        if(confirm == 'Y'){
-            System.out.print("Name: ");
-            String name = sc.nextLine();
-
-            System.out.print("IC No: ");
-            String icNo = sc.nextLine();
-
-            System.out.print("Gender: ");
-            String gender= sc.nextLine();
-
-            System.out.print("Date Of Birth: ");
-            String dateOfBirth = sc.nextLine();
-            Date dob = ConvertToDate(dateOfBirth);  // still figured out how to convert
-
-            System.out.print("Phone No: ");
-            String phoneNo= sc.nextLine();
-
-            System.out.print("Do you confirm want to register to be a member (Y= Yes N= No): ");
-            char doubleConfirm = sc.nextLine().charAt(0);
-            if(doubleConfirm == 'Y'){
-                System.out.println("You had success to add a member!!");
-                totalMember++;
-            }
-        }
-
-
-    }
-
-    // Convert String to Date
-    public Date ConvertToDate(String dateOfBirth)throws Exception{
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
-
-        return dob;
-    }
+    
 
     // Calculate the age (required or not)
     

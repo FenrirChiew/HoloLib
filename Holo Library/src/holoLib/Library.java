1d07e22bd9c0f4149e84a86ccf245ecfdedab923
zhilin1112 <chriskong1112@gmail.com>
2021-08-08 12:12:23 +0800

    update librarian and library.java

--- Holo Library/src/holoLib/Library.java
+++ Holo Library/src/holoLib/Library.java
@@ -1,57 +1,21 @@
 package holoLib;
 
-import java.util.Scanner;
-import java.util.Date;
-import java.text.SimpleDateFormat;
-
 public class Library {
-
-
-
-
-
-
-
-    // Register Membership method 
-    public Member registerMembership(){
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Do you want to register a membership (Y= yes N= No)? ");
-        char confirm = sc.nextLine().charAt(0);
-
-        if(confirm == 'Y'){
-            System.out.print("Name: ");
-            String name = sc.nextLine();
-
-            System.out.print("IC No: ");
-            String icNo = sc.nextLine();
-
-            System.out.print("Gender: ");
-            String gender= sc.nextLine();
-
-            System.out.print("Date Of Birth: ");
-            String dateOfBirth = sc.nextLine();
-            Date dob = ConvertToDate(dateOfBirth);  // still figured out how to convert
-
-            System.out.print("Phone No: ");
-            String phoneNo= sc.nextLine();
-
-            System.out.print("Do you confirm want to register to be a member (Y= Yes N= No): ");
-            char doubleConfirm = sc.nextLine().charAt(0);
-            if(doubleConfirm == 'Y'){
-                System.out.println("You had success to add a member!!");
-                
-            }
-        }
-
+    private Account account;
+    private Facility facility;
+    private ReadingMaterial readingMaterial;
+    
+    //Constructor without arguments
+    public Library(){
 
     }
 
-    // Convert String to Date
-    public Date ConvertToDate(String dateOfBirth)throws Exception{
-        Date dob = new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
-
-        return dob;
+    //Constructor with arguments
+    public Library(Account account, Facility facility, ReadingMaterial readingMaterial){
+        this.account = account;
+        this.facility = facility;
+        this.readingMaterial = readingMaterial;
     }
+
+    
 }

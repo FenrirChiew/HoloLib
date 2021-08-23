b6da26e02d4c0fd9a219ea61c562a0c104fd7ff0
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-24 00:37:46 +0800

    Updated holoLib.java (changed to use object)

--- Holo Library/src/holoLib/holoLib.java
+++ Holo Library/src/holoLib/holoLib.java
@@ -5,58 +5,10 @@ import java.util.Scanner;
 public class holoLib {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Facility[] facilityList = {
-                new Area("Detective ICT Area", "FT00001", "Area",
-                        "Desktop Computer, Headphone, Printer, Air-conditional", 5.0, new Member[] {}, 100),
-                new Room("Shark Meeting Room", "FT00002", "Room", "Microphone, Speaker, White Board, Air-conditional",
-                        10.0, new Member[] {}, 20, new Schedule("SD00001")),
-                new Area("Ancient Study Area", "FT00003", "Area", "Tablet, Speaker, Projector, Air-conditional", 10.0,
-                        new Member[] {}, 120),
-                new Room("Ripper Meeting Room", "FT00004", "Room",
-                        "Laptop Computer, Microphone, Speaker, Projector, Air-conditional", 20.0, new Member[] {}, 30,
-                        new Schedule("SD00002")),
-                new Room("Bird Meeting Room", "FT00005", "Room", "Microphone, Speaker, Projector, Air-conditional",
-                        15.0, new Member[] {}, 30, new Schedule("SD00003")) };
-        LibrarySystem holoLib = new LibrarySystem(facilityList);
-        int selection = 0;
-        boolean continueInput = true;
+        LibrarySystem holoLib = new LibrarySystem();
 
-        do {
-            try {
-                cls();
-                holoLib.displayReserveMenu();
-                selection = sc.nextInt();
-                continueInput = false;
-            } catch (Exception e) {
-                sc.nextLine();
-                System.out.print("\n\tInvalid selection! Please try agian...");
-                sc.nextLine();
-            }
-        } while (continueInput);
-
-<<<<<<< HEAD
-=======
-        LibrarySystem.Logo();
-        sc.nextLine();
->>>>>>> 354440535c45958fa98a17fb060a1f49bdf81585
-        cls();
-        sc.nextLine();
-
-        switch (selection) {
-            case 0:
-                System.out.print("\n\tReturn to last stage. Press Enter to continue...");
-                sc.nextLine();
-                cls();
-                break;
-            case 1:
-                holoLib.displayFacility();
-                System.out.print("\n\tReturn to last stage. Press Enter to continue...");
-                sc.nextLine();
-                cls();
-        }
-
-
-
+        holoLib.Logo();
+        
         sc.close();
     }
 

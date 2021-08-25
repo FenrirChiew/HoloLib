da8e631b817f3f4637b90fb07c7b2bf1fa57f491
yeesoon12 <86587298+yeesoon12@users.noreply.github.com>
2021-08-25 20:45:15 +0800

    Updated Payment

--- Holo Library/src/holoLib/Payment.java
+++ Holo Library/src/holoLib/Payment.java
@@ -1,10 +1,10 @@
 package holoLib;
 
-public class Payment {
-    
-    // Pay the money of borrowing books,  returning the balance
-    // For the code outside, if balance returned is same as the balance before -> Insuffient balance, need to top up
-    public float payBorrow(float payment, float balance){
+public interface Payment {
+    float payBorrow(float payment, float balance);
+}
+
+/*
         if (balance < payment) {
             System.out.println("Insuffient balance. Please top up the balance before borrowing the book.");
             return balance;
@@ -13,11 +13,4 @@ public class Payment {
             System.out.println("Your borrow payment has been paid");
             return balance - payment;
         }
-    }
-
-    // reload, renew membership card
-    // public float ??
-
-    // Pay penalty
-    // public float ??
-}
+*/

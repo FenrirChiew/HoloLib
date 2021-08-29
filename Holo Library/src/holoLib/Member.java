ac1382aa7b6bdb0371f300c95b85b0aafbacd1d2
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-29 18:24:21 +0800

    Updated Member.java

--- Holo Library/src/holoLib/Member.java
+++ Holo Library/src/holoLib/Member.java
@@ -3,52 +3,79 @@ package holoLib;
 //import java.util.Scanner;
 import java.util.GregorianCalendar;
 
-public class Member extends People {
-    /********** Properties **********/
-    private String memberID; // Member ID
-    private LibraryCard libraryCard; // Member Library Card
-    private static int totalMember = 0; // Total Number of Member
-    private static int nextMemberID = 0001;
+public class Member extends Borrower {
+	/********** Properties **********/
+	private String memberID;
+	private static int totalMembers = 0;
+	private static final int MAX_BORROW = 5;
 
-    /********** Constructors **********/
-    public Member() {
-        this("", "", null, "", "", null);
-    }
+	/********** Constructors **********/
+	public Member() {
+		this("", "", "", null, "", null);
+	}
 
-    public Member(String name, String gender, GregorianCalendar dateOfBirth, String icNo, String phoneNo, LibraryCard libraryCard) {
-        super(name, gender, dateOfBirth, icNo, phoneNo);
-        this.memberID = String.format("M%04d", nextMemberID);
-        this.libraryCard = libraryCard;
-        totalMember++;
-        nextMemberID++;
-    }
+	public Member(String name, String icNO, String gender, GregorianCalendar dateOfBirth, String phoneNO,
+			LibraryCard libraryCard) {
+		super(name, icNO, gender, dateOfBirth, phoneNO, libraryCard);
+		this.memberID = String.format("MB%03d", totalMembers + 1);
+		totalMembers++;
+	}
 
-    /********** Accessors & Mutators **********/
-    public String getMemberID() {
-        return memberID;
-    }
+	/********** Accessors & Mutators **********/
+	public String getMemberID() {
+		return memberID;
+	}
 
-    public LibraryCard getLibraryCard() {
-        return libraryCard;
-    }
+	public void setMemberID(String memberID) {
+		this.memberID = memberID;
+	}
 
-    public void setLibraryCard(LibraryCard libraryCard) {
-        this.libraryCard = libraryCard;
-    }
+	public static int getTotalMembers() {
+		return totalMembers;
+	}
 
-    public static int getTotalMember() {
-        return totalMember;
-    }
+	/********** Methods **********/
+	@Override
+	public void displayCardHolderDetails() {
+		System.out.println("Library Card Holder Details");
+		System.out.println("===========================");
+		System.out.println("Name           : " + name);
+		System.out.println("Member ID      : " + memberID);
+		System.out.println("Card Number    : " + libraryCard.getCardNO());
+		System.out.printf("Card Balance   : %.2f\n" + libraryCard.getCardBalance());
+		System.out.print("Current Borrow : ");
 
-    public static int getNextMemberID() {
-        return nextMemberID;
-    }
+		if (libraryCard.getCurrentBorrowed().length == 0) {
+			System.out.println("N/A");
+		} else {
+			for (int i = 0; i < libraryCard.getCurrentBorrowed().length; i++) {
+				System.out.printf("\n                 %d. %s (%s)", i + 1,
+						libraryCard.getCurrentBorrowed()[i].getMaterialTitle(),
+						libraryCard.getCurrentBorrowed()[i].getMaterialID());
+			}
 
-    /********** Methods **********/
-    // toString() method
-    @Override
-    public String toString() {
-        return super.toString() + "\nMember ID: " + memberID + libraryCard.toString();
-    }
+			System.out.printf("\n                 (Total Borrow = %d units)\n",
+					libraryCard.getCurrentBorrowed().length);
+		}
+	}
+
+	@Override
+	public void borrowBook(String pinNO, Material book) {
+		if (!(this.libraryCard.validatePinNO(pinNO))) {
+			System.out.println("\n\tInvalid Pin Number!\n");
+		} else {
+			if (libraryCard.getCurrentBorrowed().length >= MAX_BORROW) {
+				System.out.printf("\n\tYou have reached the Borrow Limit (%d)!\n\n", MAX_BORROW);
+			} else {
+				this.libraryCard.addCurrentBorrowed(book);
+			}
+		}
+	}
+
+	// toString() method
+	@Override
+	public String toString() {
+		return super.toString() + "\nMember ID: " + memberID + libraryCard.toString();
+	}
 
 }

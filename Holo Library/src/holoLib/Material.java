54d93b87f055952e86086b555b6e4bbef245bfcd
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-29 18:30:23 +0800

    Updated Material.java

--- Holo Library/src/holoLib/Material.java
+++ Holo Library/src/holoLib/Material.java
@@ -3,85 +3,95 @@ package holoLib;
 import java.util.GregorianCalendar;
 
 public class Material {
-	/********** Properties **********/
-	protected String materialTitle;
-	protected String materialID;
-	protected String materialAuthor;
-	protected String materialPublisher;
-	protected GregorianCalendar materialPublicationDate;
-	private static int totalMaterials = 0;
-	private static final int MAX_GRACE_PERIOD_IN_DAY = 30;
+    /********** Properties **********/
+    protected String materialTitle;
+    protected String materialID;
+    protected String materialAuthor;
+    protected String materialPublisher;
+    protected GregorianCalendar materialPublicationDate;
+    protected double materialPrice;
+    private static int totalMaterials = 0;
+    private static final int MAX_GRACE_PERIOD_IN_DAY = 30;
 
-	/********** Constructors **********/
-	protected Material() {
-		this("", "", "", "", null);
-		totalMaterials++;
-	}
+    /********** Constructors **********/
+    protected Material() {
+        this("", "", "", "", null, 0.0);
+        totalMaterials++;
+    }
 
-	protected Material(String materialTitle, String materialID, String materialAuthor, String materialPublisher,
-			GregorianCalendar materialPublicationDate) {
-		this.materialTitle = materialTitle;
-		this.materialID = materialID;
-		this.materialAuthor = materialAuthor;
-		this.materialPublisher = materialPublisher;
-		this.materialPublicationDate = materialPublicationDate;
-		totalMaterials++;
-	}
+    protected Material(String materialTitle, String materialID, String materialAuthor, String materialPublisher,
+            GregorianCalendar materialPublicationDate, double materialPrice) {
+        this.materialTitle = materialTitle;
+        this.materialID = materialID;
+        this.materialAuthor = materialAuthor;
+        this.materialPublisher = materialPublisher;
+        this.materialPublicationDate = materialPublicationDate;
+        this.materialPrice = materialPrice;
+        totalMaterials++;
+    }
 
-	/********** Accessors & Mutators **********/
-	public String getMaterialTitle() {
-		return materialTitle;
-	}
+    /********** Accessors & Mutators **********/
+    public String getMaterialTitle() {
+        return materialTitle;
+    }
 
-	public void setMaterialTitle(String materialTitle) {
-		this.materialTitle = materialTitle;
-	}
+    public void setMaterialTitle(String materialTitle) {
+        this.materialTitle = materialTitle;
+    }
 
-	public String getMaterialID() {
-		return materialID;
-	}
+    public String getMaterialID() {
+        return materialID;
+    }
 
-	public void setMaterialID(String materialID) {
-		this.materialID = materialID;
-	}
+    public void setMaterialID(String materialID) {
+        this.materialID = materialID;
+    }
 
-	public String getMaterialAuthor() {
-		return materialAuthor;
-	}
+    public String getMaterialAuthor() {
+        return materialAuthor;
+    }
 
-	public void setMaterialAuthor(String materialAuthor) {
-		this.materialAuthor = materialAuthor;
-	}
+    public void setMaterialAuthor(String materialAuthor) {
+        this.materialAuthor = materialAuthor;
+    }
 
-	public String getMaterialPublisher() {
-		return materialPublisher;
-	}
+    public String getMaterialPublisher() {
+        return materialPublisher;
+    }
 
-	public void setMaterialPublisher(String materialPublisher) {
-		this.materialPublisher = materialPublisher;
-	}
+    public void setMaterialPublisher(String materialPublisher) {
+        this.materialPublisher = materialPublisher;
+    }
 
-	public GregorianCalendar getMaterialPublicationDate() {
-		return materialPublicationDate;
-	}
+    public GregorianCalendar getMaterialPublicationDate() {
+        return materialPublicationDate;
+    }
 
-	public void setMaterialPublicationDate(GregorianCalendar materialPublicationDate) {
-		this.materialPublicationDate = materialPublicationDate;
-	}
+    public void setMaterialPublicationDate(GregorianCalendar materialPublicationDate) {
+        this.materialPublicationDate = materialPublicationDate;
+    }
 
-	public static int getTotalMaterials() {
-		return totalMaterials;
-	}
+    public double getMaterialPrice() {
+        return materialPrice;
+    }
 
-	public static int getMAX_GRACE_PERIOD_IN_DAY() {
-		return MAX_GRACE_PERIOD_IN_DAY;
-	}
+    public void setMaterialPrice(double materialPrice) {
+        this.materialPrice = materialPrice;
+    }
 
-	/********** Methods **********/
-	@Override
-	public String toString() {
-		return "Material Title: " + materialTitle + "\nMaterial ID: " + materialID + "\nMaterial Author: "
-				+ materialAuthor + "\nMaterial Publisher: " + materialPublisher + "\nMaterial Publication Date: "
-				+ materialPublicationDate;
-	}
+    public static int getTotalMaterials() {
+        return totalMaterials;
+    }
+
+    public static int getMAX_GRACE_PERIOD_IN_DAY() {
+        return MAX_GRACE_PERIOD_IN_DAY;
+    }
+
+    /********** Methods **********/
+    @Override
+    public String toString() {
+        return "Material Title: " + materialTitle + "\nMaterial ID: " + materialID + "\nMaterial Author: "
+                + materialAuthor + "\nMaterial Publisher: " + materialPublisher + "\nMaterial Publication Date: "
+                + materialPublicationDate + "\nMaterial Price: " + materialPrice;
+    }
 }

b5c1aad6989691dde2068def53d3d36d645c2cb7
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-08-31 10:19:55 +0800

    Updated Borrower.java

--- Holo Library/src/holoLib/Borrower.java
+++ Holo Library/src/holoLib/Borrower.java
@@ -3,39 +3,39 @@ package holoLib;
 import java.util.GregorianCalendar;
 
 public abstract class Borrower {
-    /********** Properties **********/
-    protected String name;
-    protected String icNO;
-    protected String gender;
-    protected GregorianCalendar dateOfBirth;
-    protected String phoneNO;
-    protected LibraryCard libraryCard;
+	/********** Properties **********/
+	protected String name;
+	protected String icNO;
+	protected String gender;
+	protected GregorianCalendar dateOfBirth;
+	protected String phoneNO;
+	protected LibraryCard libraryCard;
 
-    /********** Constructors **********/
-    protected Borrower() {
-        this("", "", "", null, "", null);
-    }
+	/********** Constructors **********/
+	protected Borrower() {
+		this("", "", "", null, "", null);
+	}
 
-    protected Borrower(String name, String icNO, String gender, GregorianCalendar dateOfBirth, String phoneNO,
-            LibraryCard libraryCard) {
-        this.name = name;
-        this.icNO = icNO;
-        this.gender = gender;
-        this.dateOfBirth = dateOfBirth;
-        this.phoneNO = phoneNO;
-        this.libraryCard = libraryCard;
-    }
+	protected Borrower(String name, String icNO, String gender, GregorianCalendar dateOfBirth, String phoneNO,
+			LibraryCard libraryCard) {
+		this.name = name;
+		this.icNO = icNO;
+		this.gender = gender;
+		this.dateOfBirth = dateOfBirth;
+		this.phoneNO = phoneNO;
+		this.libraryCard = libraryCard;
+	}
 
-    /********** Methods **********/
-    public abstract void displayCardHolderDetails();
+	/********** Methods **********/
+	public abstract void displayBorrowerDetails();
 
-    public abstract void borrowBook(String pinNO, Material book);
+	public abstract void borrowBook(String pinNO, Material book);
 
-    // toString() method
-    @Override
-    public String toString() {
-        return "Name: " + name + "\nIC Number: " + icNO + "\nGender: " + gender + "Date of Birth: " + dateOfBirth
-                + "\nPhone Number: " + phoneNO;
-    }
+	// toString() method
+	@Override
+	public String toString() {
+		return "Name: " + name + "\nIC Number: " + icNO + "\nGender: " + gender + "Date of Birth: " + dateOfBirth
+				+ "\nPhone Number: " + phoneNO;
+	}
 
 }

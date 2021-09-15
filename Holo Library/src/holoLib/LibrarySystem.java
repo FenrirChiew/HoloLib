ed15b3a2b2d249693a97b2207944d64424b60860
cYuanQi <cyq.chongyuanqi@gmail.com>
2021-09-15 14:52:57 +0800

    Updated LibrarySystem.java.

--- Holo Library/src/holoLib/LibrarySystem.java
+++ Holo Library/src/holoLib/LibrarySystem.java
@@ -331,6 +331,7 @@ public class LibrarySystem {
 			}
 
 		}
+
 		System.out.println("+========================================+===================+====================+================+");
 		System.out.println("Total count book borrowed: " + count);
 

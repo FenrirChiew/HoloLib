9223872ef91c368dea4c32abcabc313fd6424919
yeesoon12 <86587298+yeesoon12@users.noreply.github.com>
2021-09-15 17:00:33 +0800

    Test

--- Recycle Bin/TestLogo.java
+++ Recycle Bin/TestLogo.java
@@ -1,7 +1,53 @@
 package holoLib;
 
 public class TestLogo {
-    public static void main(String[] args) {
+
+/*
+
+// Library that need to import
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+// Codes are below
+
+	private static int recordCount = 0;
+	private String[] loginID = new String[100];
+	private LocalDateTime[] loginTime = new LocalDateTime[100];
+	private LocalDateTime[] logoutTime = new LocalDateTime[100];
+	private boolean isLogin = false;
+	private boolean isLogout = false;
+	DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+	// need to be call everytime the user login/logout + need to set isLogin / isLogout to true before it is called
+	public void RecordTime() {
+		if (isLogin) {
+			loginID[recordCount] = currentLoggedUser.getLibrarianID();
+			loginTime[recordCount] = LocalDateTime.now();
+			isLogin = false;
+		}
+
+		if (isLogout) {
+			logoutTime[recordCount] = LocalDateTime.now();
+			isLogout = false;
+			recordCount++;
+		}
+	}
+
+	// Used to display login record
+    public void displayLoginRecord() {
+        System.out.println("+==========+==========================+===========================+");
+		System.out.println("| Login ID |        Login Time        |        Logout Time        |");
+		System.out.println("+==========+==========================+===========================+");
+		for (int i = 0; i < recordCount; i++)
+			System.out.printf("| %-8s | " + dtf.format(loginTime[recordCount]) + " | " + dtf.format(logoutTime[recordCount]) + " |");
+		System.out.println("+==========+==========================+===========================+");
+    }
+
+*/
+
+
+	public static void main(String[] args) {
         System.out.println("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
 		System.out.println("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⠠⠤⠤⠤⠤⠤⠤⠤⠤⠤⠐⠒⠒⢄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");
 		System.out.println("⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⡠⠤⠄⠒⠒⠂⠤⣀⠄⠄⡠⠒⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⠒⢄⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄");

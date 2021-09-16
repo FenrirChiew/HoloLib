fdeda3317313987339bb6144c0330716f9536998
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2021-09-16 13:38:00 +0800

    Correction on logout message

--- Holo Library/src/holoLib/holoLib/holoLib.java
+++ Holo Library/src/holoLib/holoLib/holoLib.java
@@ -594,7 +594,7 @@ public class holoLib {
 							break;
 					}
 				} while (selection1 != 0);
-				System.out.println("\n\tBack To Home. Press Enter To Continue...");
+				System.out.println("\n\tYou had been Logged Out. Press Enter To Continue...");
 				sc.nextLine();
 				cls();
 			}
